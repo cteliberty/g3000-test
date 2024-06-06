810e1f8b570b074341c53d3b10dcaa7b847c8aba
Clément Thoret <clement.thoret@eliberty.fr>
2024-06-06 16:11:18 +0200

    fix link

--- src/components/organisms/header.tsx
+++ src/components/organisms/header.tsx
@@ -11,13 +11,13 @@ const Header : FC<Header> = (props) => {
 
   return (
     <div className="header" style={{display: 'flex'}}>
-      <a href="http://localhost:3000/fr">Home</a>
+      <a href="/fr">Home</a>
       <ul style={{listStyle: 'none', display: 'flex', margin: 0}}>
         <li>
-          <a href="http://localhost:3000/fr/page/mentions-legales">Mention legale</a>
+          <a href="/fr/page/mentions-legales">Mention legale</a>
         </li>
         <li>
-          <a href="http://localhost:3000/fr/billetterie/">billetterie</a>
+          <a href="/fr/billetterie/">billetterie</a>
         </li>
       </ul>
 
